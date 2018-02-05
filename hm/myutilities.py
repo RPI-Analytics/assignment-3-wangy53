0a80f12a68df9a338d61f9e24e81db41d05cc63b
Yichuan <wangy53@rpi.edu>
2018-02-05 18:36:44 -0500

    function

---
+++ hm/myutilities.py
@@ -0,0 +1,4 @@
+def divby2(x):
+    '''Create a function divby2 that accepts a list and returns all values from that list that are divisible by 2. '''
+    divby2 = [i for i in x if i%2==0]
+    return divby2
